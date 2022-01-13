f80137f2eee1200fefbfb866dca71aca7cd81598
Daniel Haag <danni.haag91@gmail.com>
2022-01-13 16:44:01 -0300

    primeiro commit

---
+++ ThreadEmExecucao/Program.cs
@@ -0,0 +1,23 @@
+﻿Main();
+
+static void Main()
+{
+    Thread t1 = new Thread(ThreadRepeticao);
+    t1.Start();
+
+    for (int i = 0; i < 1000; i++)
+    {
+        Console.WriteLine("Main: " + i);
+    }
+
+    Console.ReadKey();
+}
+
+
+static void ThreadRepeticao()
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        Console.WriteLine("Num: " + i);
+    }
+}
